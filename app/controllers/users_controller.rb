3ea46127f37c91a3dbb247db68255eef7d3f6590
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-07-09 15:32:47 -0400

    user signup form and controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,22 @@
 class UsersController < ApplicationController
 
+   use Rack::Flash
+
+  get '/signup' do
+    erb :'users/signup'
+  end
+
+  post '/signup' do
+    if !User.find_by(username: params[:username]) && params[:password] != "" && params[:email] != "" && params[:username] != ""
+      @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+      session[:user_id] = @user.id
+      redirect "/users/#{@user.id}"
+    else
+      flash[:message] = "Please create an account."
+      redirect '/signup'
+  end
+
+  end
+
 
 end
