db4a9559511d5d1a18f76aa9023cb5589451e1c4
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-07-09 16:06:22 -0400

    new list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,14 @@
 class ListsController < ApplicationController
 
+  get '/new' do
+    erb:'/lists/new'
+  end
+
+  post '/lists' do
+    @list = List.create(user_id: session[:user_id])
+    @list.mountains << Mountain.create(name: params["mountain"]["name"])
+    @list.save
+    redirect to "users/#{@list.user_id}"
+  end
 
 end
