03f88b411180f504efe9525ecc1eacddeeeecbe5
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-07-09 14:55:55 -0400

    controller files and application controller class

---
+++ app/controllers/mountains_controller.rb
@@ -0,0 +1,21 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+
+  Rack::MethodOverride
+
+  configure do
+   set :public_folder, 'public'
+   set :views, 'app/views'
+  end
+
+ configure do
+   enable :sessions
+   set :session_secret, "secret"
+ end
+
+ get '/' do
+   erb :index
+ end
+
+end
